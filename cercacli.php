810f5921bd80665e38f3281afb72e8dfb9d01182
bellanimacasadibari <bellanimasabino@yahoo.it>
2018-01-12 11:59:48 +0100

    Update cercacli.php

--- cercacli.php
+++ cercacli.php
@@ -5,14 +5,14 @@ $password=""; // Mysql password
 $db_name="my_setlist"; //Nome del Database
 $tbl_name="members"; // Nome della Tabella
 // Procedimento per connettersi al Database
-mysql_connect("$host", "$username", "$password")or die("cannot connect");
-mysql_select_db("$db_name")or die("cannot select DB"); 
+mysqli_connect("$host", "$username", "$password")or die("cannot connect");
+mysqli_select_db("$db_name")or die("cannot select DB"); 
 // Nome utente e password inviate attraverso il form
 $myusername=$_POST['myusername'];
 $sql="SELECT * FROM $tbl_name WHERE username='$myusername' and tipo='Cliente'";
-$result=mysql_query($sql);
+$result=mysqli_query($sql);
 // Mysql_num_row is counting table row
-$count=mysql_num_rows($result);
+$count=mysqli_num_rows($result);
 // If result matched $myusername and $mypassword, table row must be 1 row
 if($count==1){
 // Register $myusername, $mypassword and redirect to file "login_success.php"+
@@ -29,4 +29,4 @@ if(isset($_POST['myusername'])){
 else {
 echo "Attenzione username errato";
 }}
-?> 
+?> 
