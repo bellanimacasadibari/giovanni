891ac0e4e1a70c3b0909dc6e6561bbf82244eae3
bellanimacasadibari <bellanimasabino@yahoo.it>
2018-01-13 18:26:51 +0100

    Delete login_success.php

--- login_success.php
+++
@@ -1,7 +0,0 @@
- <?php
-// Controlla se la sessione Ã¨ stata registrata, altrimenti rimanda alla pagina di login
-session_start();
-if(!isset($_SESSION[myusername])){
-header("location:main_login.php");
-} 
-?> 
